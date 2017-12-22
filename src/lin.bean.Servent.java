b20074f4b6000e0f4d78447569d17b5d33344505
AzureVolition <31586543+AzureVolition@users.noreply.github.com>
2017-12-22 15:37:39 +0800

    Create lin.bean.Servent.java

---
+++ src/lin.bean.Servent.java
@@ -0,0 +1,26 @@
+package bean;
+
+public class Servent {
+	private Integer atk;
+	private Integer hp;
+	private String name;
+	public Integer getAtk() {
+		return atk;
+	}
+	public void setAtk(Integer atk) {
+		this.atk = atk;
+	}
+	public Integer getHp() {
+		return hp;
+	}
+	public void setHp(Integer hp) {
+		this.hp = hp;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
